b90ff7568f901498caa3a34cc2a7a47ddbe2c324
Hugo Lemos da Silva <72999426+Hugollemos@users.noreply.github.com>
2024-02-03 02:51:25 -0300

    docker.sh

---
+++ intalation_docker.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+docker --version
+sudo chmod 777 /var/run/docker.sock ou sudo usermod -aG docker $USER
